b8b5c858d27373ce9fff030d656b5b085af171b1
shakuntalagit <129251712+shakuntalagit@users.noreply.github.com>
2023-07-06 18:49:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 //Setting up middleware
-app.use(cors());
+app.use(cors(
+     {
+    origin:'https://exercise-mern-app.onrender.com',
+  }
+));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
